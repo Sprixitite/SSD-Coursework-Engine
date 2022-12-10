05397f9b005de0e9065b79dc545f705980c9a9a4
Sprixitite <91488389+Sprixitite@users.noreply.github.com>
2022-12-10 01:57:06 +0000

    GameWindow render function

--- src/windowing/GameWindow.cs
+++ src/windowing/GameWindow.cs
@@ -1,5 +1,7 @@
 using System;
 
+using Engine.Graphics;
+
 namespace Engine.Windowing {
 
     public class GameWindow : System.Windows.Forms.Form {
@@ -10,11 +12,15 @@ namespace Engine.Windowing {
 
         private System.Drawing.BufferedGraphicsContext buffered_graphics_context;
 
-        internal void render() {
+        internal void render(Camera cam, Tri3D[] triangles) {
 
-            System.Drawing.BufferedGraphics render_buffer = buffered_graphics_context.Allocate(this.CreateGraphics(), new System.Drawing.Rectangle(System.Drawing.Point.Empty, this.Size));
+            System.Drawing.BufferedGraphics render_buffer = buffered_graphics_context.Allocate(this.CreateGraphics(), this.DisplayRectangle);
 
-            
+            foreach (Tri3D tri in triangles) {
+                render_buffer.Graphics.FillPolygon(new System.Drawing.SolidBrush(Color.Green), tri.project_onto(cam).to_screenspace(new Vector2(this.Width, this.Height)));
+            }
+
+            render_buffer.Render();
 
         }
 
